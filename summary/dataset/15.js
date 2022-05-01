5459639adc4d041ece0a0a9c51f8ed52bc23f90b
Howard Zhang <75195784+Howardzhangdqs@users.noreply.github.com>
2022-05-01 21:23:16 +0800

    Add files via upload

---
+++ summary/dataset/15.js
@@ -0,0 +1,12 @@
+var data = `宝玉见北静王，面如美玉，目似明星，水溶则称宝玉面若春花，目如点漆，又要看宝玉的玉，大加称赞向贾政说将来或许“雏凤清于老凤声”，然后勉励宝玉用功，并赠与宝玉一串御赐念珠。
+一路上无大事，因凤姐嫌铁槛寺下榻不方便，提前联系了馒头庵也就是水月庵，因为馒头做的好得此名。秦钟，宝玉与小徒弟智能儿玩耍。众人散去之后，静虚老尼求凤姐一事，长安县张财主女儿金哥被府太爷的小舅子李衙内看上来求亲，无奈金哥此前已经许配给长安守备的公子，两家都抢着要娶金哥，张家不愿退亲，李家又不止，打起了官司，希望荣府暗中帮忙摆平这件事。
+凤姐听了，知道事情也不大，但是太太们如今不管这样的事。禁不住老尼劝说，凤姐来了兴致，想起一计策让张家拿3000两银子来。
+秦钟趁黑无人，偷偷到后面房中，寻到智能儿，二人按捺不住云雨起来。恰此时有一人默不作声地进来，唬的二人不敢动，后那人忍不住笑出来，听声知道是宝玉，智能趁黑逃跑，秦钟央求宝玉不要告诉别人。
+次日一早，贾母打发人来看宝玉，叫他们没事就早回家，宝玉知道秦钟恋着智能，都不肯回去，求着凤姐再住一日。
+凤姐命来旺媳妇悄悄地假借贾琏嘱托，修书一封连夜送往长安府，两日办妥。一切安置停当，凤姐又到铁槛寺照望一番，众人都回家，只有宝珠执意不肯回，贾珍派妇女相伴。<div class="image-package">
+<div class="image-container" style="max-width: 660px; max-height: 511px;">
+<div class="image-container-fill" style="padding-bottom: 77.42%;"></div>
+<div class="image-view" data-width="660" data-height="511"><img data-original-src="//upload-images.jianshu.io/upload_images/2121856-4fdf32c1d0c2e2cd.jpg" data-original-width="660" data-original-height="511" data-original-format="image/jpeg" data-original-filesize="65148"></div>
+</div>
+<div class="image-caption">图片发自简书App(源于网络)</div>
+</div>`
